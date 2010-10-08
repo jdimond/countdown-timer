1839c07fddc8b377df54990f72bfc29423174211
jonny <jonny@dimond.de>
2010-10-08 19:03:01 +0200

    Initial Record
    Ignore-this: f042fa661e3d6ac0c86ccbe098c577e4
    
    darcs-hash:20101008170301-7525e-bca232155ffe242e0a95d364489666800f153e0e

---
+++ src/de/dimond/countdowntimer/CountdownTimerAppWidgetProvider.java
@@ -0,0 +1,303 @@
+/*
+ * Copyright (C) 2010 Jonathan Dimond
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.dimond.countdowntimer;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class CountdownTimerAppWidgetProvider extends AppWidgetProvider {
+
+    public static final String INTENT_DATA_WIDGET_ID = "WIDGET_ID";
+    public static final String INTENT_ALARM_ALERT = "de.dimond.countdowntimer.INTENT_ALARM_ALERT";
+
+    private static final String TAG = "CountdownTimerAppWidgetProvider";
+
+    private static final String VIBRATE_KEY = "VIBRATE";
+    private static final String INSISTENT_KEY = "INSISTENT";
+
+    private static final boolean LOGD = true;
+
+    private static AppWidgetManager m_widgetManager;
+    private static Context m_context;
+    private static Map<Integer, RemoteViews> m_remoteViews = new HashMap<Integer, RemoteViews>();
+    private static Map<Integer, CountdownTask> m_countdownTasks = new HashMap<Integer, CountdownTask>();
+    private static final Map<Integer, Long> m_alarms = new HashMap<Integer, Long>();
+    private static Integer m_currentAlarmWidgetId = -1;
+
+    private static final Intent ALARM_INTENT = new Intent(INTENT_ALARM_ALERT);
+
+    private static <K, V> Map.Entry<K, V> smallestValue(Map<K, V> map) {
+
+        if (map.size() == 0) {
+            return null;
+        }
+
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            @SuppressWarnings("unchecked")
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return ((Comparable<V>) o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        return list.get(0);
+    }
+
+    private class CountdownTask extends TimerTask {
+
+        private final AppWidgetManager m_appWidgetManager;
+        private final RemoteViews m_views;
+        private final int m_widgetId;
+        private final long m_duration;
+        private long m_startTime;
+        private final boolean m_isSilent;
+
+        private final Timer m_timer;
+
+        private int m_lastRemainingTime = -1;
+
+        public CountdownTask(AppWidgetManager manager, RemoteViews views, int widgetId, int duration, boolean silent) {
+            m_appWidgetManager = manager;
+            m_views = views;
+            m_widgetId = widgetId;
+            /* Add 500msecs to compensate delay from alarm */
+            m_duration = duration * 1000 + 500;
+            m_isSilent = silent;
+
+            m_timer = new Timer();
+        }
+
+        public void start() {
+            m_timer.scheduleAtFixedRate(this, 0, 200);
+            m_startTime = SystemClock.elapsedRealtime();
+        }
+
+        public boolean isSilent() {
+            return m_isSilent;
+        }
+
+        public void reset() {
+            m_timer.cancel();
+            m_views.setTextViewText(R.id.timer_text, m_context.getText(R.string.timer_uninitialised));
+            m_appWidgetManager.updateAppWidget(m_widgetId, m_views);
+        }
+
+        @Override
+        public void run() {
+            int remainingTime = (int) ((m_duration - (SystemClock.elapsedRealtime() - m_startTime)) / 1000);
+            if (remainingTime <= 0) {
+                m_timer.cancel();
+                remainingTime = 0;
+            }
+
+            /* only update the view if anything has changed */
+            if (m_lastRemainingTime == remainingTime) {
+                return;
+            }
+            m_lastRemainingTime = remainingTime;
+
+            int seconds = remainingTime % 60;
+            int minutes = (remainingTime / 60) % 60;
+            int hours = remainingTime / 3600;
+
+            String time = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+            m_views.setTextViewText(R.id.timer_text, time);
+            m_appWidgetManager.updateAppWidget(m_widgetId, m_views);
+
+        }
+
+    }
+
+    public void showNotification(int id, boolean isSilent, boolean vibrate, boolean insistent) {
+        Notification n = new Notification(R.drawable.stat_notify_alarm, m_context.getString(R.string.timer_expired),
+                System.currentTimeMillis());
+
+        n.defaults = Notification.DEFAULT_LIGHTS;
+        if (!isSilent) {
+            n.defaults |= Notification.DEFAULT_SOUND;
+        }
+        if (vibrate) {
+            n.vibrate = new long[] { 0, 500, 200, 500, 200, 750 };
+        }
+
+        n.flags = Notification.FLAG_AUTO_CANCEL;
+        if (insistent) {
+            n.flags |= Notification.FLAG_INSISTENT;
+        }
+
+        n.setLatestEventInfo(m_context, m_context.getString(R.string.timer_expired),
+                m_context.getString(R.string.click_to_remove),
+                PendingIntent.getBroadcast(m_context, 0, new Intent(), 0));
+
+        NotificationManager manager = (NotificationManager) m_context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(id);
+        manager.notify(id, n);
+    }
+
+    @Override
+    public void onUpdate(Context context, final AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int n = appWidgetIds.length;
+
+        m_widgetManager = appWidgetManager;
+
+        m_context = context;
+
+        for (int i = 0; i < n; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            Intent intent = new Intent(context, NewTimerActivity.class);
+            /*
+             * The Android system currently reuses Intents if they match,
+             * ignoring extras. To ensure that we have an unique intent for each
+             * widget we add a pseudo data field with an unique id
+             */
+            intent.setData(Uri.parse("unique://" + appWidgetId));
+            intent.putExtra(INTENT_DATA_WIDGET_ID, appWidgetId);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.countdown_timer_widget);
+            views.setOnClickPendingIntent(R.id.timer_text, pendingIntent);
+
+            if (LOGD)
+                Log.d(TAG, "Putting RemoteViews into HashMap with ID " + appWidgetId + "!");
+            m_remoteViews.put(appWidgetId, views);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    private void scheduleAlarm() {
+        Map.Entry<Integer, Long> nextAlarm = smallestValue(m_alarms);
+        AlarmManager manager = (AlarmManager) m_context.getSystemService(Context.ALARM_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(m_context, 0, ALARM_INTENT,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+
+        /* No alarm left so cancel any remaining Intent */
+        if (nextAlarm == null) {
+            manager.cancel(pendingIntent);
+            m_currentAlarmWidgetId = -1;
+            return;
+        }
+
+        manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, nextAlarm.getValue(), pendingIntent);
+        m_currentAlarmWidgetId = nextAlarm.getKey();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (LOGD)
+            Log.d(TAG, "Received Intent: " + intent.getAction());
+        if (intent.getAction().equals(NewTimerActivity.INTENT_NEW_TIMER)) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                int widgetId = extras.getInt(INTENT_DATA_WIDGET_ID, -1);
+                int duration = extras.getInt(NewTimerActivity.INTENT_DATA_DURATION, -1);
+                boolean silent = extras.getBoolean(NewTimerActivity.INTENT_DATA_SILENT, false);
+                if (widgetId == -1 || duration == -1) {
+                    Log.w(TAG, "Received invalid intent!");
+                    return;
+                }
+
+                if (LOGD)
+                    Log.d(TAG, "Received Intent with widgetId=" + widgetId + " and duration=" + duration);
+
+                if (m_countdownTasks.containsKey(widgetId)) {
+                    if (LOGD)
+                        Log.d(TAG, "Stopping old timer with ID " + widgetId);
+                    m_countdownTasks.get(widgetId).cancel();
+                    m_countdownTasks.remove(widgetId);
+                }
+                RemoteViews views = m_remoteViews.get(widgetId);
+                if (views == null) {
+                    Log.e(TAG, "RemoteView not in HashMap!");
+                    return;
+                }
+                CountdownTask countdownTask = new CountdownTask(m_widgetManager, views, widgetId, duration, silent);
+                m_countdownTasks.put(widgetId, countdownTask);
+                countdownTask.start();
+                m_alarms.put(widgetId, SystemClock.elapsedRealtime() + duration * 1000);
+                scheduleAlarm();
+            }
+        } else if (intent.getAction().equals(NewTimerActivity.INTENT_CANCEL_TIMER)) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                int widgetId = extras.getInt(INTENT_DATA_WIDGET_ID, -1);
+                if (widgetId == -1) {
+                    Log.w(TAG, "Received invalid intent!");
+                    return;
+                }
+                if (m_countdownTasks.containsKey(widgetId)) {
+                    m_countdownTasks.get(widgetId).reset();
+                    m_countdownTasks.remove(widgetId);
+                    if (m_alarms.remove(widgetId) == null) {
+                        Log.w(TAG, "Inconsistent data! No Alarm with widgetId " + widgetId + " in m_alarms!");
+                    }
+                    scheduleAlarm();
+                }
+            }
+        } else if (intent.getAction().equals(INTENT_ALARM_ALERT)) {
+            if (m_currentAlarmWidgetId == -1) {
+                Log.w(TAG, "Invalid Intent received! m_currentAlarmWidgetId was -1!");
+                return;
+            }
+
+            boolean isSilent = m_countdownTasks.get(m_currentAlarmWidgetId).isSilent();
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(m_context);
+            boolean vibrate = preferences.getBoolean(VIBRATE_KEY, true);
+            boolean insistent = preferences.getBoolean(INSISTENT_KEY, false);
+            showNotification(m_currentAlarmWidgetId, isSilent, vibrate, insistent);
+
+            if (m_alarms.remove(m_currentAlarmWidgetId) == null) {
+                Log.w(TAG, "Inconsistent data! No Alarm with widgetId " + m_currentAlarmWidgetId + " in m_alarms!");
+            }
+
+            if (m_countdownTasks.containsKey(m_currentAlarmWidgetId)) {
+                m_countdownTasks.get(m_currentAlarmWidgetId).reset();
+            }
+
+            scheduleAlarm();
+        } else {
+            /* Only call if this intent is not from us */
+            super.onReceive(context, intent);
+        }
+    }
+}
---
+++ src/de/dimond/countdowntimer/NewTimerActivity.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2010 Jonathan Dimond
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.dimond.countdowntimer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import com.android.example.NumberPicker;
+
+public class NewTimerActivity extends Activity implements OnClickListener {
+
+    public static final String INTENT_NEW_TIMER = "de.dimond.countdowntimer.INTENT_NEW_TIMER";
+    public static final String INTENT_CANCEL_TIMER = "de.dimond.countdowntimer.INTENT_CANCEL_TIMER";
+    public static final String INTENT_DATA_DURATION = "DURATION";
+    public static final String INTENT_DATA_SILENT = "SILENT";
+
+    public static final String HOURS_KEY = "HOURS";
+    public static final String MINUTES_KEY = "MINUTES";
+    public static final String SECONDS_KEY = "SECONDS";
+    public static final String SILENT_KEY = "SILENT";
+
+    public static final String TAG = "NewTimerActivity";
+    public static final boolean LOGD = true;
+
+    private int m_widgetId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.new_timer);
+
+        NumberPicker pickerHours = (NumberPicker) findViewById(R.id.hours);
+        NumberPicker pickerMinutes = (NumberPicker) findViewById(R.id.minutes);
+        NumberPicker pickerSeconds = (NumberPicker) findViewById(R.id.seconds);
+
+        SharedPreferences prefrences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        pickerHours.setRange(0, 23);
+        pickerHours.setCurrent(prefrences.getInt(HOURS_KEY, 0));
+
+        pickerMinutes.setRange(0, 59);
+        pickerMinutes.setFormatter(NumberPicker.TWO_DIGIT_FORMATTER);
+        pickerMinutes.setCurrent(prefrences.getInt(MINUTES_KEY, 1));
+
+        pickerSeconds.setRange(0, 59);
+        pickerSeconds.setFormatter(NumberPicker.TWO_DIGIT_FORMATTER);
+        pickerSeconds.setCurrent(prefrences.getInt(SECONDS_KEY, 0));
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.silent);
+        checkBox.setChecked(prefrences.getBoolean(SILENT_KEY, false));
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        m_widgetId = intent.getIntExtra(CountdownTimerAppWidgetProvider.INTENT_DATA_WIDGET_ID, -1);
+        if (LOGD)
+            Log.d(TAG, "Received Intent with Widget ID=" + m_widgetId);
+        if (m_widgetId == -1) {
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Intent i = new Intent();
+            i.setClass(this, SettingsActivity.class);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(findViewById(R.id.start_button))) {
+            NumberPicker pickerHours = (NumberPicker) findViewById(R.id.hours);
+            NumberPicker pickerMinutes = (NumberPicker) findViewById(R.id.minutes);
+            NumberPicker pickerSeconds = (NumberPicker) findViewById(R.id.seconds);
+
+            CheckBox checkBox = (CheckBox) findViewById(R.id.silent);
+
+            int hours = pickerHours.getCurrent();
+            int minutes = pickerMinutes.getCurrent();
+            int seconds = pickerSeconds.getCurrent();
+
+            boolean silent = checkBox.isChecked();
+
+            saveState(hours, minutes, seconds, silent);
+
+            Intent intent = new Intent(INTENT_NEW_TIMER);
+            intent.putExtra(INTENT_DATA_DURATION, hours * 3600 + minutes * 60 + seconds);
+            intent.putExtra(INTENT_DATA_SILENT, silent);
+            intent.putExtra(CountdownTimerAppWidgetProvider.INTENT_DATA_WIDGET_ID, m_widgetId);
+
+            sendBroadcast(intent);
+
+            finish();
+        } else if (v.equals(findViewById(R.id.cancel_button))) {
+            Intent intent = new Intent(INTENT_CANCEL_TIMER);
+            intent.putExtra(CountdownTimerAppWidgetProvider.INTENT_DATA_WIDGET_ID, m_widgetId);
+
+            sendBroadcast(intent);
+
+            finish();
+        }
+    }
+
+    private void saveState(int hours, int minutes, int seconds, boolean silent) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putInt(HOURS_KEY, hours);
+        editor.putInt(MINUTES_KEY, minutes);
+        editor.putInt(SECONDS_KEY, seconds);
+        editor.putBoolean(SILENT_KEY, silent);
+
+        editor.commit();
+    }
+
+}
